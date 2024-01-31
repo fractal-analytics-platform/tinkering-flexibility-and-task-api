df6d49ca100cc036fe9378536724fb58c1a37803
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-01-31 14:21:26 +0100

    Add cellpose and image_meta

--- example_C/tasks.py
+++ example_C/tasks.py
@@ -2,11 +2,19 @@ from pathlib import Path
 from typing import Any
 from typing import Optional
 
+from termcolor import cprint
+
+
+def print(x):
+    return cprint(x, "cyan")
+
 
 def create_ome_zarr(
     *,
     root_dir: str,
     image_dir: str,
+    buffer: Optional[dict[str, Any]] = None,
+    image_meta: Optional[dict[str, Any]] = None,
 ) -> dict:
     """
     TBD
@@ -36,7 +44,11 @@ def create_ome_zarr(
     # Prepare output metadata
     out = dict(
         new_images=[
-            dict(path=f"{plate_zarr_name}/{image_relative_path}")
+            dict(
+                path=f"{plate_zarr_name}/{image_relative_path}",
+                plate=plate_zarr_name,
+                well="_".join(image_relative_path.split("/")[:2]),
+            )
             for image_relative_path in image_relative_paths
         ],
         buffer=dict(
@@ -44,6 +56,9 @@ def create_ome_zarr(
                 f"{plate_zarr_name}/A/01/0": f"{image_dir}/figure_A01.tif",
                 f"{plate_zarr_name}/A/02/0": f"{image_dir}/figure_A02.tif",
             },
+        ),
+        new_filters=dict(
+            plate=plate_zarr_name,
         ),
     )
     print("[create_ome_zarr] END")
@@ -55,6 +70,7 @@ def yokogawa_to_zarr(
     root_dir: str,
     component: str,
     buffer: dict[str, Any],
+    image_meta: Optional[dict[str, Any]] = None,
 ) -> dict:
     """
     TBD
@@ -85,6 +101,7 @@ def illumination_correction(
     root_dir: str,
     component: str,
     buffer: Optional[dict[str, Any]] = None,
+    image_meta: Optional[dict[str, Any]] = None,
 ) -> dict:
     print("[illumination_correction] START")
     print(f"[illumination_correction] {root_dir=}")
@@ -93,12 +110,36 @@ def illumination_correction(
     new_component = f"{component}_corr"
     print(f"[illumination_correction] {new_component=}")
 
+    if image_meta is None:
+        image_meta = {}
+
     out = dict(
         new_images=[
-            dict(path=new_component, illumination_correction=True),
+            dict(
+                path=new_component,
+                illumination_correction=True,
+                **image_meta,
+            ),
         ],
         new_filters=dict(illumination_correction=True),
     )
     print(f"[illumination_correction] {out=}")
     print("[illumination_correction] END")
+    return out
+
+
+def cellpose_segmentation(
+    *,
+    root_dir: str,
+    component: str,
+    buffer: dict[str, Any],
+    image_meta: Optional[dict[str, Any]] = None,
+) -> dict:
+    print("[cellpose_segmentation] START")
+    print(f"[cellpose_segmentation] {root_dir=}")
+    print(f"[cellpose_segmentation] {component=}")
+
+    out = dict()
+    print(f"[cellpose_segmentation] {out=}")
+    print("[cellpose_segmentation] END")
     return out
