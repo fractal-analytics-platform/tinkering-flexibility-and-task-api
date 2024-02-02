5f50eb0754604a9ec93c9bafc1700c53de60ed9b
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-02-02 11:44:57 +0100

    Add example of `Task.new_default_filters`

--- example_D/db.py
+++ example_D/db.py
@@ -12,9 +12,18 @@ def get_task(id: int) -> Task:
     TASKS = [
         Task(id=1, function=create_ome_zarr),
         Task(id=2, function=yokogawa_to_zarr, meta=dict(parallel=True)),
-        Task(id=3, function=illumination_correction, meta=dict(parallel=True)),
+        Task(
+            id=3,
+            function=illumination_correction,
+            meta=dict(parallel=True),
+            new_default_filters=dict(illumination_correction=True),
+        ),
         Task(id=4, function=cellpose_segmentation, meta=dict(parallel=True)),
-        Task(id=5, function=copy_ome_zarr, meta=dict(combine_components=True)),
+        Task(
+            id=5,
+            function=copy_ome_zarr,
+            meta=dict(combine_components=True),
+        ),
     ]
     task = next(t for t in TASKS if t.id == id)
     return task
