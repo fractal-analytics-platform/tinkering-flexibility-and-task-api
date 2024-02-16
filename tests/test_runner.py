49afeeb00b13469dddc2427fe61ed20dc0551b45
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-02-16 09:08:50 +0100

    revert last commit

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -24,7 +24,6 @@ def test_max_parallelization_list_size(N: int):
     dataset = Dataset(
         id=1,
         root_dir="/tmp/invalid",
-        images=parallelization_list,
         parallelization_list=parallelization_list,
     )
     wf_task_list = [
