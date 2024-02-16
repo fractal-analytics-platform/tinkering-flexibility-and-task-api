97cb3c5cd7e02f8557e1094a5d75204ee3569283
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-02-16 14:40:44 +0100

    Add test_unit_aux_functions.py

--- tests/test_apply_workflow.py
+++ tests/test_apply_workflow.py
@@ -1,9 +1,7 @@
-import pytest
 from devtools import debug
 from models import Dataset
 from models import Task
 from models import WorkflowTask
-from runner import _validate_parallelization_list_valid
 from runner import apply_workflow
 
 from tests.tasks_for_tests import create_images_from_scratch
@@ -41,63 +39,3 @@ def test_single_parallel_task_no_parallization_list():
     ]
     dataset_out = apply_workflow(wf_task_list=task_list, dataset=dataset_in)
     debug(dataset_out.image_paths)
-
-
-def test_unit_validate_parallelization_list():
-
-    # Missing path
-    PARALLELIZATION_LIST = [dict()]
-    CURRENT_IMAGE_PATHS = []
-    with pytest.raises(ValueError):
-        _validate_parallelization_list_valid(
-            parallelization_list=PARALLELIZATION_LIST,
-            current_image_paths=CURRENT_IMAGE_PATHS,
-        )
-
-    # Path not in current image paths
-    PARALLELIZATION_LIST = [dict(path="asd")]
-    CURRENT_IMAGE_PATHS = []
-    with pytest.raises(ValueError):
-        _validate_parallelization_list_valid(
-            parallelization_list=PARALLELIZATION_LIST,
-            current_image_paths=CURRENT_IMAGE_PATHS,
-        )
-
-    # Invalid buffer attributes
-    PARALLELIZATION_LIST = [dict(path="asd", buffer={})]
-    CURRENT_IMAGE_PATHS = ["asd"]
-    with pytest.raises(ValueError):
-        _validate_parallelization_list_valid(
-            parallelization_list=PARALLELIZATION_LIST,
-            current_image_paths=CURRENT_IMAGE_PATHS,
-        )
-
-    # Invalid `buffer`` attribute
-    PARALLELIZATION_LIST = [dict(path="asd", buffer={})]
-    CURRENT_IMAGE_PATHS = ["asd"]
-    with pytest.raises(ValueError):
-        _validate_parallelization_list_valid(
-            parallelization_list=PARALLELIZATION_LIST,
-            current_image_paths=CURRENT_IMAGE_PATHS,
-        )
-
-    # Invalid `root_dir` attribute
-    PARALLELIZATION_LIST = [dict(path="asd", root_dir="/something")]
-    CURRENT_IMAGE_PATHS = ["asd"]
-    with pytest.raises(ValueError):
-        _validate_parallelization_list_valid(
-            parallelization_list=PARALLELIZATION_LIST,
-            current_image_paths=CURRENT_IMAGE_PATHS,
-        )
-
-    # Valid call
-    PARALLELIZATION_LIST = [
-        dict(path="asd", parameter=1),
-        dict(path="asd", parameter=2),
-        dict(path="asd", parameter=3),
-    ]
-    CURRENT_IMAGE_PATHS = ["asd"]
-    _validate_parallelization_list_valid(
-        parallelization_list=PARALLELIZATION_LIST,
-        current_image_paths=CURRENT_IMAGE_PATHS,
-    )
