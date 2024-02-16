87de0df4bc97f422438f96dddeef1e7f77634a23
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-02-16 15:23:05 +0100

    Add test-models

---
+++ tests/test_models.py
@@ -0,0 +1,13 @@
+from devtools import debug
+from models import Task
+
+
+def test_model_task():
+    def _dummy_function():
+        pass
+
+    NEW_FILTERS = dict(attribute=True)
+    task = Task(function=_dummy_function, new_filters=NEW_FILTERS)
+    debug(task.new_filters)
+    debug(NEW_FILTERS)
+    assert task.new_filters == NEW_FILTERS
